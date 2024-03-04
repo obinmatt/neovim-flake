6d007bdeeb3b521329900f0aeb03251fb61f935f
obinmatt <dev.simplify740@slmails.com>
2024-03-03 20:49:38 -0500

    move autocmds around

--- modules/autocmds.lua
+++ modules/autocmds.lua
@@ -1,18 +1,3 @@
--- disable indentscope for certain file types
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = {
-		"help",
-		"notify",
-		"undotree",
-		"toggleterm",
-		"oil_preview",
-		"oil",
-	},
-	callback = function()
-		vim.b.miniindentscope_disable = true
-	end,
-})
-
 -- highlight when yanking text
 vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight when yanking (copying) text",
--- modules/plugins/mini.lua
+++ modules/plugins/mini.lua
@@ -12,3 +12,17 @@ require("mini.indentscope").setup({
 	symbol = "│",
 	options = { try_as_border = true },
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = {
+		"help",
+		"notify",
+		"undotree",
+		"toggleterm",
+		"oil_preview",
+		"oil",
+	},
+	callback = function()
+		vim.b.miniindentscope_disable = true
+	end,
+})
