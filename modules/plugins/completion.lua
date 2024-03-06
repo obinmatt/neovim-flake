40e266b6154a66d13505ef69744e3d9d24adc5d5
obinmatt <dev.simplify740@slmails.com>
2024-03-05 22:42:35 -0500

    simplify cmp mappings

--- modules/plugins/completion.lua
+++ modules/plugins/completion.lua
@@ -17,7 +17,7 @@ cmp.setup({
 		return not context.in_treesitter_capture("comment") and not context.in_syntax_group("Comment")
 	end,
 	completion = {
-		completeopt = "menu,menuone,preview,noselect",
+		completeopt = "menu,menuone,noselect",
 		keyword_length = 2,
 	},
 	snippet = {
@@ -28,40 +28,27 @@ cmp.setup({
 	mapping = cmp.mapping.preset.insert({
 		["<C-n>"] = cmp.mapping.select_next_item(),
 		["<C-p>"] = cmp.mapping.select_prev_item(),
-		["<C-b>"] = cmp.mapping.scroll_docs(-4),
-		["<C-f>"] = cmp.mapping.scroll_docs(4),
-		["<C-Space>"] = cmp.mapping.complete({}),
-		["<CR>"] = cmp.mapping.confirm({
-			behavior = cmp.ConfirmBehavior.Replace,
-			select = true,
-		}),
-		["<Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			elseif luasnip.expand_or_locally_jumpable() then
+		["<C-y>"] = cmp.mapping.confirm({ select = true }),
+		["<C-l>"] = cmp.mapping(function()
+			if luasnip.expand_or_locally_jumpable() then
 				luasnip.expand_or_jump()
-			else
-				fallback()
 			end
 		end, { "i", "s" }),
-		["<S-Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item()
-			elseif luasnip.locally_jumpable(-1) then
+		["<C-h>"] = cmp.mapping(function()
+			if luasnip.locally_jumpable(-1) then
 				luasnip.jump(-1)
-			else
-				fallback()
 			end
 		end, { "i", "s" }),
 	}),
-	sources = {
+	sources = cmp.config.sources({
 		{ name = "nvim_lsp" },
-		{ name = "luasnip" },
-		{ name = "buffer" },
-		{ name = "path" },
-	},
+		{ name = "luasnip", max_item_count = 3 },
+		{ name = "buffer", max_item_count = 5 },
+		{ name = "path", max_item_count = 3 },
+	}),
 	formatting = {
 		format = lspkind.cmp_format({
+			mode = "symbol_text",
 			maxwidth = 50,
 			ellipsis_char = "...",
 		}),
