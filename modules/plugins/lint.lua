b649e2ca75c9bc03e9440fda6ce6b0dacb31bd4c
obinmatt <dev.simplify740@slmails.com>
2025-02-22 16:17:20 -0500

    add debounce to linter

--- modules/plugins/lint.lua
+++ modules/plugins/lint.lua
@@ -5,12 +5,27 @@ require("lint").linters_by_ft = {
 	typescriptreact = { "eslint_d" },
 	html = { "eslint_d" },
 	css = { "eslint_d" },
-	json = { "eslint_d" },
 }
+
+local debounce = function(ms, fn)
+	local timer = vim.uv.new_timer()
+	return function(...)
+		local argv = { ... }
+		timer:start(ms, 0, function()
+			timer:stop()
+			vim.schedule_wrap(fn)(unpack(argv))
+		end)
+	end
+end
+
+local lint = function()
+	-- only run the linter in buffers that are modifiable
+	if vim.opt_local.modifiable:get() then
+		require("lint").try_lint()
+	end
+end
 
 vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave" }, {
 	group = vim.api.nvim_create_augroup("nvim-lint", { clear = true }),
-	callback = function()
-		require("lint").try_lint()
-	end,
+	callback = debounce(100, lint),
 })
