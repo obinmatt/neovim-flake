cdaf2f4da8548dc67a0e3f78089543450a2c65be
obinmatt <dev.simplify740@slmails.com>
2025-12-03 17:53:52 -0500

    picker files+grep from buffer directory

--- modules/plugins/snacks.lua
+++ modules/plugins/snacks.lua
@@ -29,9 +29,12 @@ local map = function(keys, func, desc)
 	vim.keymap.set("n", keys, func, { desc = desc })
 end
 
-map("<leader>ff", snacks.picker.files, "Find files")
+-- stylua: ignore start
+map("<leader>ff", snacks.picker.files, "Find files (cwd)")
+map("<leader>fF", function() snacks.picker.files({ cwd = vim.fn.expand('%:p:h') }) end, "Find files (buffer dir)")
 map("<leader>fr", snacks.picker.recent, "Find recent")
-map("<leader>fg", snacks.picker.grep, "Find grep")
+map("<leader>fg", snacks.picker.grep, "Find grep (cwd)")
+map("<leader>fG", function() snacks.picker.grep({ cwd = vim.fn.expand('%:p:h') }) end, "Find grep (buffer dir)")
 map("<leader>gg", snacks.lazygit.open, "Lazygit")
 map("<leader>z", snacks.zen.zen, "Zen mode")
 map("<leader><leader>", function()
